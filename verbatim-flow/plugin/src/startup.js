7d347becd24b3abe95a77fb58c3355558ee485e3
kabir-shetty <kabir.n.shetty@gmail.com>
2026-05-30 17:17:25 -0500

    Fixed error of speech times not being printed to the spreadsheet

--- verbatim-flow/plugin/src/startup.js
+++ verbatim-flow/plugin/src/startup.js
@@ -1,12 +1,11 @@
-window.Asc.plugin.init = function () {
-    this.callCommand(function () {
-        var oSheet = Api.GetActiveSheet();
-        oSheet.GetRangeByNumber(0, 0).SetValue("1AC");
-        oSheet.GetRangeByNumber(0, 1).SetValue("1NC");
-        oSheet.GetRangeByNumber(0, 2).SetValue("2AC");
-        oSheet.GetRangeByNumber(0, 3).SetValue("Block");
-        oSheet.GetRangeByNumber(0, 4).SetValue("1AR");
-        oSheet.GetRangeByNumber(0, 5).SetValue("2NR");
-        oSheet.GetRangeByNumber(0, 6).SetValue("2AR");
-    });
-}
+//variables
+let worksheet = Application.GetActiveSheet();
+
+// Sets speech names
+worksheet.GetRange("A1").SetValue("1AC");
+worksheet.GetRange("B1").SetValue("1NC");
+worksheet.GetRange("C1").SetValue("2AC");
+worksheet.GetRange("D1").SetValue("Block");
+worksheet.GetRange("E1").SetValue("1AR");
+worksheet.GetRange("F1").SetValue("2NR");
+worksheet.GetRange("G1").SetValue("2AR");
