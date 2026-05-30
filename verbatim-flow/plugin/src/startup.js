94c7c9526325ae4aa6e8195bf136ca56f8020471
kabir-shetty <kabir.n.shetty@gmail.com>
2026-05-30 17:57:16 -0500

    idfk atp

--- verbatim-flow/plugin/src/startup.js
+++ verbatim-flow/plugin/src/startup.js
@@ -1,11 +1,15 @@
-//variables
-let worksheet = Application.GetActiveSheet();
+// //variables
+// let worksheet = Api.GetActiveSheet();
 
-// Sets speech names
-worksheet.GetRange("A1").SetValue("1AC");
-worksheet.GetRange("B1").SetValue("1NC");
-worksheet.GetRange("C1").SetValue("2AC");
-worksheet.GetRange("D1").SetValue("Block");
-worksheet.GetRange("E1").SetValue("1AR");
-worksheet.GetRange("F1").SetValue("2NR");
-worksheet.GetRange("G1").SetValue("2AR");
+// // Sets speech names
+// worksheet.GetRange("A1").SetValue("1AC");
+// worksheet.GetRange("B1").SetValue("1NC");
+// worksheet.GetRange("C1").SetValue("2AC");
+// worksheet.GetRange("D1").SetValue("Block");
+// worksheet.GetRange("E1").SetValue("1AR");
+// worksheet.GetRange("F1").SetValue("2NR");
+// worksheet.GetRange("G1").SetValue("2AR");
+
+window.Asc.plugin.init = function () {
+    window.Asc.plugin.executeMethod("InputText", ["Hello World"]);
+}
